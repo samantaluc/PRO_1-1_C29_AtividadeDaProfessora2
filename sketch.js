e2881a9f19411e876e45b239a406911a6e254c7d
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-05-22 17:07:04 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,15 +6,12 @@ const Constraint = Matter.Constraint;
 const Body = Matter.Body;
 const Composites = Matter.Composites;
 const Composite = Matter.Composite;
-
 let engine;
 let world;
 var ground;
-var fruit,rope;
-var fruit_con;
-
-function setup() 
-{
+var fruit,rope; // adicionar
+var fruit_con; // adicionar
+function setup() {
   //cria o "fundo"
   createCanvas(500,700);
   frameRate(80);
@@ -22,40 +19,27 @@ function setup()
   world = engine.world;
   //cria o chão
   ground = new Ground(200,680,600,20);
-
-  //cria uma nova corda com 7 segmentos e na posição X/Y
+  //cria uma nova corda com 7 segmentos e na posição X/Y <<<<<<< adicionar
   rope = new Rope(10,{x:245,y:30});
-
-  //cria a "bola"
+  //cria a "bola"  <<<<<<< adicionar
   fruit = Bodies.circle(300,300,20);
-  
-  //adiciona fisica usando o matter.js
+  //adiciona fisica usando o matter.js <<<<<<< adicionar
   Matter.Composite.add(rope.body,fruit);
-
-  //cria um link entre a corda e a bola
+  //cria um link entre a corda e a bola  <<<<<<< adicionar
   fruit_con = new Link(rope,fruit);
-
   rectMode(CENTER);
   ellipseMode(RADIUS);
-  textSize(50)
-  
+  textSize(50);
 }
-
-function draw() 
-{
+function draw() {
   //cor do "fundo"
   background(51);
-
-  //desenha a corda
+  //desenha a corda <<<<<<< adicionar
   rope.show();
-
-  //desenha a "bola" com o tamanho X=30 e Y=30
+  //desenha a "bola" com o tamanho X=30 e Y=30 <<<<<<< adicionar
   ellipse(fruit.position.x,fruit.position.y,30,30);
-
   //atualiza em tempo real a fisica
   Engine.update(engine);
-
   //desenha o chão
   ground.show();
- 
 }
